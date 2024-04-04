acb53f10faf4ac1b0d1ea195ec03023bb538cb7a
Kaweekorn <ou.sayhello@gmail.com>
2024-04-04 18:14:27 +0700

    new

--- humidity.js
+++ humidity.js
@@ -5,7 +5,7 @@ let output = document.getElementById('output1')
 function printHumidity()
 {
     let number= Number(input.value);
-    let Humidity= -230.673+683.328*number-448.630*(number**2)
+    let Humidity= (-230.673)+(683.328*number)-(448.630*(number*number))
     console.log(Humidity)
     output.innerHTML= Humidity
 }
